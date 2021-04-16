f829738efe9d94861eacb3e3733ab21cba24f68a
Marc Fielding <marcfielding@hotmail.com>
2021-04-17 00:21:37 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # swagger-mock-header
 Express middleware for generating mocking responses based on swagger.
+
+The purpose of this example is to show a super lightweight implementation of an express middleware that will return a response body based on the x-mock-header value found in openapi.yaml specifications.
+
+A much more heavyweight example of this can be found here: https://github.com/APIDevTools/swagger-express-middleware
+
+### Getting Started
+
+Consider the following specification:
+
+```
+openapi: 3.0.3
+info:
+  title: Appeals Service API Documentation
+  description:
+    API to <ul><li>create, retrieve and modify planning appeals and </li><li>retrieve local planning authorities.</li></ul>
+  version: 1.0.3
+  license:
+    name: MIT
+    url: 'https://opensource.org/licenses/MIT'
+servers:
+  - url: 'http://localhost:3000/'
+paths:
+  '/api/v1/appeals':
+    post:
+      summary: Creates a new appeal
+      description: A newly created appeal will have a DRAFT state and all subsections will have NOT STARTED state. The id will be a uuid and the returned representation of the new appeal will be valid so it can be submitted as a PUT payload 'as is'.
+      tags:
+        - Appeals
+      responses:
+        '201':
+          description: Returns a newly created appeal
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/Appeal'
+```              
+
+By calling the above endpoint with x-mock-header set to the response code you want it'll return the values from the content section using the "example" values in the component.
+
+The is useful to unblock frontend teams/app develop teams while backend work is being implemented and also for e2e tests where you simply need to mock a response from another service.
+
---
+++ src/index.js
@@ -0,0 +1,73 @@
+const SwaggerParser = require('@apidevtools/swagger-parser');
+const fs = require('fs');
+
+let swagger;
+let initialized = false;
+let logger;
+let config;
+
+/* eslint consistent-return: 0 */
+const mockMiddleWare = (req, res, next) => {
+  if (initialized === false) {
+    logger.err('Swagger Mock middleware not initialized');
+    next();
+  }
+
+  if (req.headers['x-mock-header']) {
+    console.log('api def', swagger);
+
+    const { method } = req; // GET, POST etc
+    const requestUrl = req.baseUrl + req.path; // Path without URL params
+    const apiSpec = swagger[requestUrl]; // This gets the overarching object for that path.
+    const requestedResponse = req.headers['x-mock-header']; // this tells us if we're returning a mocked response. IE x-mock-header: 200
+    const methodSpec = apiSpec[method.toLowerCase()]; // the actual object relating to the path AND method.
+
+    const responses = Object.keys(methodSpec.responses);
+
+    // check if under responses the response code exists.
+    if (!responses.includes(method)) {
+      logger.debug(`Response code ${requestedResponse} is not included in the API spec.`);
+      return next();
+    }
+
+    // now we know it exists we need to pull out the content. Swagger parser resolves $ref's on it's own
+
+    const response = methodSpec[method]?.responses[requestedResponse].content; // TODO: rename this variable.
+
+    // nothing defined, nothing retuned.
+    if (!response) {
+      logger.err(`No mock response available for requested code. ${requestedResponse}`);
+      return next();
+    }
+
+    // return the example response from the API spec.
+    return res.json(response);
+  }
+};
+
+const init = async (conf) => {
+  try {
+    config = conf;
+
+    swagger = await SwaggerParser.validate(fs.readFileSync(config.swaggerFilePath, 'utf8'));
+
+    // some express apps have their own logging functions so.....
+    if (config.log) {
+      logger.error = config.log.err;
+      logger.warn = config.log.warn;
+      logger.debug = config.log.debug;
+      logger.info = config.log.info;
+    } else {
+      logger = console;
+    }
+
+    initialized = true;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  mockMiddleWare,
+  init,
+};
